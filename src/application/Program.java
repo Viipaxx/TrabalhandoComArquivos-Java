204ddc5810a97718fdbdac72f096d64fdc03f988
Vitor Paz <kngatuno@gmail.com>
2023-03-01 21:14:06 -0300

    Utilizando o FileReader e BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,48 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
+
 	public static void main(String[] args) {
-		// Instanciando um objeto do tipo file
-		File file = new File("C:\\Users\\kngat\\OneDrive\\Documentos\\GitHub\\TrabalhandoComArquivos-Java\\ArquivoParaSerLido.txt");
-		Scanner sc = null;
+
+		// Utilizando o fileReader e BufferedReader
+
+		String path = "C:\\Users\\kngat\\OneDrive\\Documentos\\GitHub\\TrabalhandoComArquivos-Java\\ArquivoParaSerLido.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 		
 		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+			
+			// ou
+			// br = new BufferedReader(new FileReader(path));
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		}
-		catch (IOException e) {
+		catch (IOException e ) {
 			System.out.println("Error: " + e.getMessage());
 		}
 		finally {
-			if (sc != null) {
-				sc.close();				
+			try {	
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			}
+			catch (IOException e ) {
+				System.out.println("Error: " + e.getMessage());
 			}
 		}
 	}
